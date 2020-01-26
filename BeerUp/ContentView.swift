180d7ccd0f6598e816b101c44b73c7bfd173b664
torburg <torburg@yandex.ru>
2020-01-26 18:32:38 +0300

    Initial Commit

---
+++ BeerUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeerUp
+//
+//  Created by Maksim Torburg on 26.01.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
