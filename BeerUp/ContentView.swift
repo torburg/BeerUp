ccbfda3610c6a9d629c8259b1cce56f3fc9502e5
torburg <torburg@yandex.ru>
2020-01-27 15:42:14 +0300

    App icon, simple SwiftUI ui, gitignore added

--- BeerUp/ContentView.swift
+++ BeerUp/ContentView.swift
@@ -9,13 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var beers: [Beer] = testData()
+    var colorScheme: ColorScheme = .light
+    
+    @State var presentingBeerAddForm = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                self.presentingBeerAddForm.toggle()
+            }) {
+                Text("Add Guiness")
+            }
+            .padding([.top, .bottom], 100)
+            .alert(isPresented: self.$presentingBeerAddForm) {
+                Alert(title: Text("One more Guiness?"),
+                      message: Text("New Guiness!"),
+                      dismissButton: .destructive(Text("Absolutely")))
+            }
+            List(beers) { beer in
+                Image(systemName: "photo")
+                VStack(alignment: .leading) {
+                    Text("Hello, Beer User!")
+                    Text("New text")
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
+                    Text("\(beer.bid)")
+                    Text("\(beer.beerName)")
+                }
+            }
+        }
     }
 }
 
+// MARK: - WTF is this?
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(beers: testData(),colorScheme: .dark)
     }
 }
---
+++ BeerUp/Model/Beer.swift
@@ -0,0 +1,39 @@
+//
+//  Beer.swift
+//  BeerUp
+//
+//  Created by Maksim Torburg on 26.01.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import Foundation
+
+struct Beer: Identifiable {
+    var id = UUID()
+    
+    let bid: Int
+    let beerName: String
+//    let beerLabel: URL
+//    let beerAbv: Double
+//    let beerIbu: Int
+//    let beerSlug: String
+//    let beerStyle: String
+//    let beer_description: String
+//    let createdAt: Date
+//    let authRating: Double
+//    let wishList: Bool
+//    let ratingScore: Double
+}
+
+#if DEBUG
+let testData = {
+    return countBeer()
+}
+func countBeer() -> [Beer] {
+    var beers = [Beer]()
+    for i in 1...10 {
+        beers.append(Beer(bid: i, beerName: "\(i) Guiness Please"))
+    }
+    return beers
+}
+#endif
